1c92c7c64dd8e2985dd37524d73ad7b61891562e
TirthPShah <cptirthp.shah@gmail.com>
2024-06-29 11:33:42 +0530

    Updated User Models

--- simproject/users/admin.py
+++ simproject/users/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Role, User
+
+admin.site.register(Role)
+admin.site.register(User)
--- simproject/users/models.py
+++ simproject/users/models.py
@@ -13,20 +13,12 @@ class Role(models.Model):
     def __str__(self):
         return self.role
 
-class MembershipType(models.Model):
-    type = models.CharField(max_length=100)
-    price = models.IntegerField(max_length=10, blank=False, null=False)
-
-    def __str__(self):
-        return self.type
-
 class User(BaseProfile):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     membership_start_date = models.DateField(blank=True, null=True)
     membership_end_date = models.DateField(blank=True, null=True)
-    membership_type = models.ForeignKey(MembershipType, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -35,24 +27,3 @@ class User(BaseProfile):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
-class Admin(BaseProfile):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Admin'
-        verbose_name_plural = 'Admins'
-
-class FacilityManager(BaseProfile):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Facility Manager'
-        verbose_name_plural = 'Facility Managers'
